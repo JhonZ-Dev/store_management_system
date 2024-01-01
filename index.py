9bfd9499f57201a69c466178466bc4e409d5e588
JhonZ-Dev <jhonmacias1999@gmail.com>
2023-12-31 19:42:39 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
 import os
 import pickle
+
+class Product:
+    def __init__(self, id, name, price, quantity):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.quantity = quantity
